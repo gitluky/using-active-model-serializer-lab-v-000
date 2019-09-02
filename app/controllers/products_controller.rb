36ea062f236217cce6084a513a4b4768690d1a8b
Jonathan Luk <jluk87@gmail.com>
2019-09-02 14:03:10 +0000

    Automatically backed up by Learn

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,9 +1,6 @@
 class ProductsController < ApplicationController
   def index
     @products = Product.all
-    respond_to do |format|
-      format.json { render json: @products }
-    end
   end
 
   def inventory
